1c2641cab124199efe74b3467f25bbbbdd9bbcd5
tmtsmrsl <timotiusmarselo@gmail.com>
2025-04-12 18:27:53 +1200

    fix bug regarding video citation

--- fastapi_app/utils/CitationFormatter.py
+++ fastapi_app/utils/CitationFormatter.py
@@ -37,50 +37,19 @@ def _add_timestamps_to_title(title, block_ids, extra_end_time=15):
     start_time = str(datetime.timedelta(seconds=start_time))
     end_time = str(datetime.timedelta(seconds=end_time))
     return f"{title} ({(start_time)}-{end_time})"
-    
-def process_video_citation(citation, merge_threshold=60):
-    """
-    Process video citation and combine block_ids if they are continuous within 30 seconds.
-    """
-    # Extract and sort block_ids
+
+def process_video_citation(citation):
+    """Process video citation and format with timestamps."""
     block_ids = sorted(citation['block_ids'])
-    combined_citation = []
-    current_group = []
-    earliest_time = None
-
-    for block_id in block_ids:
-        if not current_group:
-            # Start a new group
-            current_group.append(block_id)
-            earliest_time = block_id
-        else:
-            # Check if the current block_id is within merge threshold (in seconds) of the last one in the group
-            last_time = current_group[-1]
-            if block_id - last_time <= merge_threshold:
-                current_group.append(block_id)
-            else:
-                # End the current group and start a new one
-                combined_citation.append({
-                    'final_url': _add_timestamp_to_url(citation['url'], earliest_time),
-                    'title': _add_timestamps_to_title(citation['title'], current_group),
-                    'old_citation_ids': citation['old_citation_ids'],
-                    'block_ids': current_group,
-                    'content_type': citation['content_type']
-                })
-                current_group = [block_id]
-                earliest_time = block_id
-
-    # Add the remaining last group
-    if current_group:
-        combined_citation.append({
-            'final_url': _add_timestamp_to_url(citation['url'], earliest_time),
-            'title': _add_timestamps_to_title(citation['title'], current_group),
-            'old_citation_ids': citation['old_citation_ids'],
-            'block_ids': current_group,
-            'content_type': citation['content_type']
-        })
-
-    return combined_citation
+    earliest_time = block_ids[0]
+    new_citation = {
+        'final_url': _add_timestamp_to_url(citation['url'], earliest_time),
+        'title': _add_timestamps_to_title(citation['title'], block_ids),
+        'old_citation_ids': citation['old_citation_ids'],
+        'block_ids': block_ids,
+        'content_type': citation['content_type']
+    }
+    return new_citation
 
 class CitationFormatter:
     @staticmethod
@@ -123,37 +92,68 @@ class CitationFormatter:
         for source in source_metadata:
             matching_ids = set(source['source_ids']).intersection(citation_ids)
             if matching_ids:
-                if source['url'] not in unique_urls:
-                    unique_urls.add(source['url'])
-                    citation = {"url": source['url'], 
-                                "title": source['title'],
-                                "old_citation_ids": [], 
-                                "block_ids": [],
-                                "content_type": source['content_type']}
-                    for source_split in source['source_splits']:
-                        if source_split['source_id'] in matching_ids:
-                            citation['old_citation_ids'].append(source_split['source_id'])
-                            # this block id is already sorted by their char start index in the source text
-                            citation['block_ids'].append(source_split['block_id'])
-                    citation_data.append(citation)
+                if source['content_type'] == 'video_transcript':
+                    # Modified: group video splits using merge_threshold
+                    splits = [s for s in source['source_splits'] if s['source_id'] in matching_ids]
+                    splits.sort(key=lambda s: s['block_id'])
+                    merge_threshold = 60  # seconds threshold for merging splits
+                    current_group = []
+                    for split in splits:
+                        if not current_group:
+                            current_group.append(split)
+                        else:
+                            if split['block_id'] - current_group[-1]['block_id'] <= merge_threshold:
+                                current_group.append(split)
+                            else:
+                                citation = {
+                                    "url": source['url'], 
+                                    "title": source['title'],
+                                    "old_citation_ids": [s['source_id'] for s in current_group], 
+                                    "block_ids": [s['block_id'] for s in current_group],
+                                    "content_type": source['content_type']
+                                }
+                                citation_data.append(citation)
+                                current_group = [split]
+                    if current_group:
+                        citation = {
+                            "url": source['url'], 
+                            "title": source['title'],
+                            "old_citation_ids": [s['source_id'] for s in current_group], 
+                            "block_ids": [s['block_id'] for s in current_group],
+                            "content_type": source['content_type']
+                        }
+                        citation_data.append(citation)
                 else:
-                    citation = next((c for c in citation_data if c['url'] == source['url']), None)
-                    for source_split in source['source_splits']:
-                        if source_split['source_id'] in matching_ids:
-                            citation['old_citation_ids'].append(source_split['source_id'])
-                            citation['block_ids'].append(source_split['block_id'])
-                            
+                    if source['url'] not in unique_urls:
+                        unique_urls.add(source['url'])
+                        citation = {"url": source['url'], 
+                                    "title": source['title'],
+                                    "old_citation_ids": [], 
+                                    "block_ids": [],
+                                    "content_type": source['content_type']}
+                        for source_split in source['source_splits']:
+                            if source_split['source_id'] in matching_ids:
+                                citation['old_citation_ids'].append(source_split['source_id'])
+                                # this block id is already sorted by their char start index in the source text
+                                citation['block_ids'].append(source_split['block_id'])
+                        citation_data.append(citation)
+                    else:
+                        citation = next((c for c in citation_data if c['url'] == source['url']), None)
+                        for source_split in source['source_splits']:
+                            if source_split['source_id'] in matching_ids:
+                                citation['old_citation_ids'].append(source_split['source_id'])
+                                citation['block_ids'].append(source_split['block_id'])
+        
+        print(citation_data)
         new_citation_data = []
         for citation in citation_data:        
             if citation['content_type'] == 'video_transcript':
-                new_citation_data.extend(process_video_citation(citation))
+                new_citation_data.append(process_video_citation(citation))
             elif citation['content_type'] == 'html_content':
                 citation['final_url'] = citation['url'] + "/block/" + ",".join(citation['block_ids'])
                 new_citation_data.append(citation)
                 
-        
-        # # Sort by the minimum value in 'citation_ids'
-        # new_citation_data = sorted(new_citation_data, key=lambda x: min(x['old_citation_ids']))
+        print(new_citation_data)
         # process the final citation url and id
         new_citation_id = 1
         for citation in new_citation_data:
